c44515e6c2ea8fb276620159f0ce84f384e009e1
Antony Palomino <antony.leo12agosto@gmail.com>
2022-12-12 22:34:15 -0500

    :pencil: resolve merge problems

--- src/components/CustomMenu/index.tsx
+++ src/components/CustomMenu/index.tsx
@@ -9,13 +9,29 @@ const CustomMenu = ({menuItems}: CustomMenuProps)=>{
   return (
     <Menu>
       <MenuButton
+        className="menu-button"
         height='4rem'
-        width='4rem'
-        icon={<HamburgerIcon height='3rem'width='3rem'/>}
+        width='4em'
+        icon={
+          <HamburgerIcon 
+            height='3rem'
+            width='3rem'
+            color='black'
+            />
+          }
         variant='outline'
+        background='none'
+        border='none'
         aria-label='Options' 
-        as={IconButton} />
-      <MenuList width='100vw'>
+        as={IconButton} 
+      />
+      <MenuList
+        className="menu-list"
+        width='100vw'
+        margin='20px'
+        display='grid'
+        gap='10px'
+      >
         {menuItems}
       </MenuList>
     </Menu>
--- src/containers/CustomNavBar/index.tsx
+++ src/containers/CustomNavBar/index.tsx
@@ -1,29 +1,90 @@
-import { Box, HStack, Image, MenuItem } from "@chakra-ui/react"
 import Link from "next/link"
+import { ChevronDownIcon } from "@chakra-ui/icons"
+import { Box, HStack, Image, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
 import { memo } from "react"
 import CustomMenu from "../../components/CustomMenu"
 
 const menuItems = [
-  {label: 'Incio', isCollapsable: false, href:'' },
-  {label: 'Nosotros', isCollapsable: true , href:'/about' },
-  {label: 'Boletin', isCollapsable: false, href:'' }
+  {label: 'Inicio', isCollapsable: false, href:'' },
+  {
+    label: 'Nosotros',
+    isCollapsable: true ,
+    href:'',
+    subLabels: [{label:'option1', href: ''}, {label:'option2', href: ''}] 
+  },
+  {label: 'Boletín', isCollapsable: false, href:'' },
+  {label: 'Politicas Juventudes', isCollapsable: false, href:'' },
+  {label: 'Capacitaciones', isCollapsable: true , href:'' },
+  {label: 'Contáctenos', isCollapsable: false, href:'' }
 ]
 
 const CustomNavBar = ()=> { 
   return (
-    <HStack justifyContent="space-between">
-      <Box>
+    <HStack
+      justifyContent="space-between"
+      margin="20px"
+    >
+      <Box
+        className="image-container"
+      >
         <Image
-          boxSize='300px'
+          boxSize='200px'
           objectFit='cover'
           height='100%' 
           alt="logo observatorio"
           src="http://drive.google.com/uc?export=view&id=1OVvy3iD6Ou-QFZIurS7S4NrqAYk9WFo7"/>
       </Box>
-      <CustomMenu menuItems={
+      <CustomMenu 
+        
+        menuItems={
         menuItems
-          .map( ({label, href}, index)=> <MenuItem key={`nav-bar-item-${index}`}>
-            <Link href={href}>{label}</Link>
+          .map( ({label, isCollapsable, href, subLabels}, index)=> <MenuItem
+            className="menu-item"
+            key={`nav-bar-item-${index}`}
+            background='none'
+            color='black'
+            border='none'
+            fontWeight='500'
+            fontSize='20px'
+          >
+            
+            
+            {isCollapsable ?
+              <Menu>
+                <MenuButton
+                  color='black'
+                  background='none'
+                  border='none'
+                  aria-label='Options'
+                  fontSize="20px"
+                >
+                  {label}
+                  <ChevronDownIcon/>
+                </MenuButton>
+                <MenuList
+                  display='grid'
+                  gap='15px'
+                  margin='10px'
+                >
+                  {subLabels?.map(({label, href}, index)=> <MenuItem
+                    key={`sub-label-${index}`}
+                    background='none'
+                    color='black'
+                    border='none'
+                    fontWeight='500'
+                    fontSize='20px'
+                    >
+                    <Link href={href}>{label}</Link>
+                  </MenuItem>
+                  )}
+
+                </MenuList>
+              </Menu>:
+              <Link href={href}>{label}</Link>
+              
+            }
+
+            
           </MenuItem>)
       }/>
     </HStack>
