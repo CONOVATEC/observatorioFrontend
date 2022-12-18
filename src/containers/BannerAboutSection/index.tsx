99ca784e1651329063a07706280de7398372b79d
luis-designs <luislozano120xd@gmail.com>
2022-12-18 18:22:04 -0500

    :sparkles: Add some section to the route about

---
+++ src/containers/BannerAboutSection/index.tsx
@@ -0,0 +1,19 @@
+import { HStack, Text, VStack } from '@chakra-ui/react'
+
+function BannerAboutSection() {
+  return (
+    <HStack paddingX={'8px'} height={'45vh'} backgroundImage={'url("https://res.cloudinary.com/df5nwnlnu/image/upload/v1671396068/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/NOSOTROS/fondo-quienes_somos_V2_xtb3e7.png")'}>
+      <VStack
+        paddingX={{ base: '10%', md: '19%' }}
+        spacing={{ base: 5, md: 10 }}
+        textColor='white'>
+        <Text fontWeight={'bold'} fontSize={'3xl'}>¿Quiénes somos?</Text>
+        <Text>
+          Somos un proyecto del Consejo Metropolitano Participación de la Juventud  de Lima, que busca promocionar datos actualizados sobre las condiciones sociodemográficas, socioeconómicas y realizar una vigilancia a la implementación de las políticas públicas en materia de juventud en el Péru.
+        </Text>
+      </VStack>
+    </HStack>
+  )
+}
+
+export default BannerAboutSection
--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -4,21 +4,21 @@ import Link from 'next/link'
 import { useCallback } from 'react'
 import { ILogoData, IMenuItem, SectionProps } from '../types'
 
-export const useRenderPropsLogosCards = ()=>{
+export const useRenderPropsLogosCards = () => {
 
-  const handleCarousel = useCallback(({url_image, name } : ILogoData, index: number) =>
-  <Box
-    aria-label={name}
-    key={`carousel-image-${index}`}
-    height={{base:'50vh', md:'1xl'}}
-    backgroundSize='50%'
-    position="relative"
-    backgroundPosition="center"
-    backgroundRepeat="no-repeat"
-    backgroundImage={`url(${url_image})`}/>
- , [])
+  const handleCarousel = useCallback(({ url_image, name }: ILogoData, index: number) =>
+    <Box
+      aria-label={name}
+      key={`carousel-image-${index}`}
+      height={{ base: '50vh', md: '1xl' }}
+      backgroundSize='50%'
+      position="relative"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      backgroundImage={`url(${url_image})`} />
+    , [])
 
- const handleStackRender = useCallback(({ url_image }: ILogoData, index: number, array:ILogoData[]) => 
+  const handleStackRender = useCallback(({ url_image }: ILogoData, index: number, array: ILogoData[]) =>
     <Box
       key={`sponsor-${index}`}
       width={`calc(96%/${array.length})`}
@@ -33,18 +33,19 @@ export const useRenderPropsLogosCards = ()=>{
   return [handleCarousel, handleStackRender]
 }
 
-export const useRenderPropsMainCards = () =>{
-  const _handleMainCardsRender = useCallback((data: SectionProps, index: number)=>{
+export const useRenderPropsMainCards = () => {
+  const _handleMainCardsRender = useCallback((data: SectionProps, index: number) => {
     const { title, image, content_body, alt_image } = data
 
     return (
-      <Card 
+      <Card
         key={`card-carousel-first-${index}`}
-        maxW={'250px'}
-        rounded={0} 
+        maxW={'270px'}
+        rounded={0}
         height={'200%'}
-        backgroundColor='Background'>
-        <CardBody backgroundColor='Background'>
+        backgroundColor='Background'
+        >
+        <CardBody backgroundColor='Background' paddingX={'15px'}>
           <Image src={image} alt={alt_image} borderRadius="lg" boxSize="100px" />
           <Stack mt="6" spacing="3">
             <Heading size="md">{title}</Heading>
@@ -56,13 +57,13 @@ export const useRenderPropsMainCards = () =>{
     )
   }, [])
 
-  const _handleStackMainCardsRender = useCallback((data: SectionProps, index: number)=>{
+  const _handleStackMainCardsRender = useCallback((data: SectionProps, index: number) => {
     const { title, image, content_body, alt_image } = data
     return (
-      <Box height={{base: '50vh', md: '40vh'}} key={`render-card-carousel-first-${index}`}>
+      <Box height={{ base: '50vh', md: '40vh' }} key={`render-card-carousel-first-${index}`}>
         <Card
-          height={'100%'} 
-          rounded={0} 
+          height={'100%'}
+          rounded={0}
           backgroundColor='Background'>
           <CardBody backgroundColor='Background'>
             <Image src={image} alt={alt_image} borderRadius="lg" boxSize="100px" />
@@ -80,22 +81,22 @@ export const useRenderPropsMainCards = () =>{
   return [_handleMainCardsRender, _handleStackMainCardsRender]
 }
 
-export const useRenderPropsMenuItems = ()=> {
-  const handlerRenderMenuItems = useCallback((menuItem: IMenuItem, index: number) =>{
+export const useRenderPropsMenuItems = () => {
+  const handlerRenderMenuItems = useCallback((menuItem: IMenuItem, index: number) => {
     const { subLabels, label, href } = menuItem
-    if(!subLabels?.length)
-     return <Link key={`menu-item-${index}`} href={href}>{label}</Link>
-    
-    return  (<Menu key={`menu-item-${index}`}>
+    if (!subLabels?.length)
+      return <Link key={`menu-item-${index}`} href={href}>{label}</Link>
+
+    return (<Menu key={`menu-item-${index}`}>
       <MenuButton>{label} <ChevronDownIcon /> </MenuButton>
       <MenuList >
-          {menuItem.subLabels?.map(({ label, href }, index) => <MenuItem
-            key={`sub-menu-item-${index}`}
-          >
-            <Link href={href}>{label}</Link>
-          </MenuItem>
-          )}
-        </MenuList>
+        {menuItem.subLabels?.map(({ label, href }, index) => <MenuItem
+          key={`sub-menu-item-${index}`}
+        >
+          <Link href={href}>{label}</Link>
+        </MenuItem>
+        )}
+      </MenuList>
     </Menu>)
   }, [])
   return [handlerRenderMenuItems]
