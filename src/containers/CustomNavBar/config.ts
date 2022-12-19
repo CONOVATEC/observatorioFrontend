63499e714b7659cbdcd29c44a6ffe6fe9a1b4ff6
Adrian <adrian.capchaq@gmail.com>
2022-12-18 22:20:05 -0500

    corrigiendo urls y nombres

--- src/containers/CustomNavBar/config.ts
+++ src/containers/CustomNavBar/config.ts
@@ -7,8 +7,8 @@ export const menuItems = [
     subLabels: [{ label: 'Sobre el Observatorio', href: '' }, { label: 'Sobre el CMPJ', href: '' }]
   },
   { label: 'Boletín', isCollapsable: false, href: '' },
-  { label: 'Politicas Juventudes', isCollapsable: false, href: '' },
-  { label: 'Capacitaciones', isCollapsable: true, href: '', subLabels: [{ label: 'Estrategia Metropolitana de Juventudes', href: '' }, { label: 'Política Nacional de Juventud', href: '' }] },
+  { label: 'Politicas Juveniles', isCollapsable: false, href: '', subLabels: [{ label: 'Estrategia Metropolitana de Juventudes', href: '' }, { label: 'Política Nacional de Juventud', href: '' }] },
+  { label: 'Capacitaciones', isCollapsable: false, href: '' },
   { label: 'Contáctenos', isCollapsable: false, href: '/contact' }
 ]
 
