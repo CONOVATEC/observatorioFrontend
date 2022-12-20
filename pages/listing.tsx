a8cbf1c70f45ee65151361e99c7af48c0f8e40c0
miguel <mendoza.ing1826@gmail.com>
2022-12-19 22:57:06 -0500

    grid noticias

--- pages/listing.tsx
+++ pages/listing.tsx
@@ -1,32 +1,13 @@
-import { CircularProgress, Text } from '@chakra-ui/react';
-import Link from 'next/link';
-import { MouseEvent } from 'react';
-import { useGetPostsApiQuery } from '../src/redux/posts/slice';
+import CustomHero from '../src/containers/CustomHero';
+import NewsSection from '../src/containers/NewsSection';
 import DefaultLayout from '../src/views/DefaultLayout';
 
 export default function Listing() {
-  const { data, isLoading}  = useGetPostsApiQuery(null)
 
   return (
     <DefaultLayout>
-      {isLoading && <CircularProgress />}
-      {!isLoading &&
-        data?.data
-          ?.map( (item: any, index: number) => {
-            const { title, id} =  item 
-
-            return (
-              <Link 
-                key={`post-${index}`}
-                href={{
-                  pathname: '/post/[id]',
-                  query: { id }
-                }}>
-                <Text>{title}</Text>
-              </Link>
-            )
-          })  
-      }
+      <CustomHero />
+      <NewsSection/>
     </DefaultLayout>
   )
 }
