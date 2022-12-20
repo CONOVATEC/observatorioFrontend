507d87ed283f702ed6fc641aa54fcd7f500cafcf
Adrian <adrian.capchaq@gmail.com>
2022-12-20 00:18:09 -0500

    editando trainings

--- pages/trainings.tsx
+++ pages/trainings.tsx
@@ -1,11 +1,24 @@
 import DirectiveSection from '../src/containers/DirectiveSection'
 import DefaultLayout from '../src/views/DefaultLayout'
+import { Image, Text, VStack } from '@chakra-ui/react'
+import BannerAboutSection from '../src/containers/BannerAboutSection'
+import GoalAboutSection from '../src/containers/GoalAboutSection'
 
-const About =() =>{
+const About = () => {
   return (
-  <DefaultLayout>
-    <DirectiveSection />
-  </ DefaultLayout>
+    <DefaultLayout>
+      <BannerAboutSection />
+      <GoalAboutSection />
+      <VStack spacing={4} my={10}>
+        <Text fontWeight={'bold'} fontSize={'2xl'}>
+          Organigrama
+        </Text>
+      </VStack>
+      <Image src='https://res.cloudinary.com/df5nwnlnu/image/upload/v1671075059/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/CAPACITACIONES/Imagen_Capacitaciones_exzw9t.png'
+        alt='organidrama-observatorio' boxSize='full'
+        objectFit='cover' />
+      <DirectiveSection />
+    </ DefaultLayout>
   )
 }
 
