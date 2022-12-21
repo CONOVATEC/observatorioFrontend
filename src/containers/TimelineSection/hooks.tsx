3a40eb7eb2ed714be47ac3c50393a4c60ae931b7
luis-designs <luislozano120xd@gmail.com>
2022-12-20 23:20:26 -0500

    :sparkles: Completed view national

--- src/containers/TimelineSection/hooks.tsx
+++ src/containers/TimelineSection/hooks.tsx
@@ -5,28 +5,29 @@ export const useNationalHandleProps = () => {
   const handleRenderCarousel = ({ image, alt, year, title, description }: SectionNationalCarouselProps, index: number) => (
     <Box
       display={'flex'}
-      alignItems={'center'}
-      height={'60vh'}>
+      height={{ base: '82vh', sm: '85vh', md: '50vh', lg: '60vh' }}>
       <HStack
         flexDir={{ base: 'column', md: 'row' }}
         key={`national-carousel-${index}`}
+        alignItems={'center'}
+        height={'100%'}
       >
         <Image
           alt={alt}
-          maxW={'40%'}
+          maxW={{ base: '70%', md: '50%', lg: '40%' }}
+          paddingLeft={{ base: '0%', lg: '5%' }}
           src={image} />
         <VStack
-          maxW={'50%'}
-          height={250}
-          justifyContent={'center'}
+          maxW={{ base: '70%', md: '50%' }}
+          height={{ base: 230, md: 300, lg: 250 }}
+          justifyContent={{ base: 'start', md: 'center' }}
           alignItems={'start'}
-          paddingLeft={'5%'}
+          paddingLeft={{ base: '0%', md: '5%' }}
         >
           <Text fontSize='3xl' color={'blue.300'}>{year}</Text>
           <Text fontSize='1xl' fontWeight={'bold'}>{title}</Text>
           <Text >{description}</Text>
         </VStack>
-
       </HStack >
     </Box>
   )
--- src/containers/TimelineSection/index.tsx
+++ src/containers/TimelineSection/index.tsx
@@ -1,4 +1,4 @@
-import { Text, VStack } from '@chakra-ui/react'
+import { Button, Image, Text, VStack } from '@chakra-ui/react'
 import ResponsiveGalleryWithCarousel from '../../views/ResponsiveGalleryWithCarousel'
 import { nationalData } from '../../config'
 import { useNationalHandleProps } from './hooks'
@@ -18,12 +18,40 @@ function TimelineSection() {
       <CaptionCarousel cards={nationalData.map(_handleRenderCarousel)} carouselProps={
         {
           display: 'block',
-          height: '60vh',
+          height: { base: '82vh', sm: '85vh', md: '50vh', lg: '60vh' },
           pl: '8%',
           pr: '8%',
         }
       } />
-
+      <VStack
+        spacing={4}
+        mt={{ base: '0%', lg: '5%' }}
+        mx={8}
+        alignItems={'start'}>
+        <Text fontWeight={'bold'} fontSize={'3xl'}>
+          ¿Qués es la política nacional de juventud?
+        </Text>
+        <Text paddingBottom={5} fontSize={'xl'}>
+          Es la respuesta del estado que orienta las acciones e intervenciones públicas en materia de juventud a fin de promover el desarrollo integral para la efectiva participación de los jóvenes en la sociedad.
+        </Text>
+        <Button borderRadius={0} colorScheme='blue' variant='outline'>
+          Descargar Política
+        </Button>
+      </VStack>
+      <VStack
+        spacing={10}
+        my={10}
+        mx={8}
+        justifyContent={'center'}>
+        <Text fontWeight={'bold'} fontSize={{ base: '2xl', md: '3xl' }}>
+          OBJETIVOS
+        </Text>
+        <Image src={'https://res.cloudinary.com/df5nwnlnu/image/upload/v1671075067/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/POL%C3%8DTICA%20NACIONAL%20DE%20LA%20JUVENTUD/Infograf%C3%ADa_con_iconos_de_los_objetivos_de_la_pol%C3%ADtica-01_nqsdpr.jpg'} alt={'infografia politica nacional'} />
+        <Image src={'https://res.cloudinary.com/df5nwnlnu/image/upload/v1671075067/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/POL%C3%8DTICA%20NACIONAL%20DE%20LA%20JUVENTUD/IAMGEN_DE_POLITICA-03_1_gjqd17.png'} alt={'politica nacional detalle'} />
+        <Button borderRadius={0} colorScheme='blue' variant='outline'>
+          Más Información
+        </Button>
+      </VStack>
     </>
   )
 }
