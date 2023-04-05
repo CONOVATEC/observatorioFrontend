9d7306f24a8cfafd7d3b28c39882053910ddc0ed
SebastianDeLaCruz <sebastiandelacruz335@gmail.com>
2023-04-05 16:03:58 -0300

    estilos al navbar agregados

--- src/containers/CustomNavBar/index.tsx
+++ src/containers/CustomNavBar/index.tsx
@@ -1,32 +1,31 @@
-import { memo } from 'react'
-import { Box, HStack } from '@chakra-ui/react'
-import { menuItems } from './config'
-import { useRenderPropsMenuItems } from '../../hooks'
-import CustomMenu from '../../components/CustomMenu'
-import ModeSwitcher from '../../components/ModeSwitcher'
-import Logo from '../../atoms/Logo'
-import style from './style.module.css'
+import { Box, HStack } from "@chakra-ui/react";
+import { memo } from "react";
+import Logo from "../../atoms/Logo";
+import CustomMenu from "../../components/CustomMenu";
+import ModeSwitcher from "../../components/ModeSwitcher";
+import { useRenderPropsMenuItems } from "../../hooks";
+import { menuItems } from "./config";
+import style from "./style.module.css";
 
 const CustomNavBar = () => {
-  const [handlerRenderMenuItems, handlerRenderMobileMenuItems] = useRenderPropsMenuItems()
+	const [handlerRenderMenuItems, handlerRenderMobileMenuItems] =
+		useRenderPropsMenuItems();
 
-  return (
-    <HStack className={style.menu} as='nav' backgroundColor='Background' >
-      <Box>
-        <Logo />
-      </Box>
-      <HStack display={{ base: 'none', md: 'flex' }}>
-        {menuItems.map(handlerRenderMenuItems)}
-        <ModeSwitcher />
-      </HStack>
-      <HStack display={{ base: 'flex', md: 'none' }}>
-        <ModeSwitcher />
-        <CustomMenu menuItems={
-          menuItems.map(handlerRenderMobileMenuItems)
-        } />
-      </HStack>
-    </HStack>
-  )
-}
+	return (
+		<HStack className={style.menu} as='nav' backgroundColor='Background'>
+			<Box>
+				<Logo />
+			</Box>
+			<HStack display={{ base: "none", md: "flex" }}>
+				{menuItems.map(handlerRenderMenuItems)}
+				<ModeSwitcher />
+			</HStack>
+			<HStack display={{ base: "flex", md: "none" }}>
+				<ModeSwitcher />
+				<CustomMenu menuItems={menuItems.map(handlerRenderMobileMenuItems)} />
+			</HStack>
+		</HStack>
+	);
+};
 
-export default memo(CustomNavBar)
+export default memo(CustomNavBar);
