af6c32255423e5ca8daed0686a1e0b141cd5846e
SebastianDeLaCruz <sebastiandelacruz335@gmail.com>
2023-04-16 01:21:21 -0300

    agregando funcion getDataInfo para los post

---
+++ pages/post/adapters/getInfoData.ts
@@ -0,0 +1,13 @@
+import { ParsedUrlQuery } from "querystring";
+
+/* Filtrar el post de la api.Devuelve el la imagen o el contendido  */
+
+export const getInfoData = (data: any, query: ParsedUrlQuery, type: string) => {
+  const post = (data?.data as Array<any>)?.find(
+    ({ id }) => String(id) === String(query.id)
+  );
+
+  if (type === "imagen") return post.imagen;
+  else if (type === "content") return post.content;
+  else return "no se encuentra disponible el dato";
+};
