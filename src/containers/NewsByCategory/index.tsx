2dbb4804dcc11937500dd427464af6bc64512679
ArturoMirZet <arturo.kjw@gmail.com>
2023-05-19 13:53:30 -0600

    Agregando funcion Ver mas y render de tarjetas

---
+++ src/containers/NewsByCategory/index.tsx
@@ -0,0 +1,126 @@
+import {
+	Box,
+	Button,
+	Flex,
+	Grid,
+	useBreakpointValue,
+	Link,
+	Text,
+	VStack,
+	Image,
+	Card,
+	CardBody,
+	Stack,
+	Heading,
+} from '@chakra-ui/react';
+import { off } from 'process';
+import { memo, useEffect, useState } from 'react';
+interface PokemonInfo {
+  name: string;
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string;
+      };
+      home: {
+        front_default: string;
+      };
+    };
+  };
+  abilities: { ability: { name: string } }[];
+  types: { type: { name: string } }[];
+}
+
+interface PokemonCardProps {
+  url: string;
+}
+export const Cards: React.FC<PokemonCardProps> = ({ url }) => {
+	const [pokemonInfo, setPokemonInfo] = useState<PokemonInfo | null>(null);
+
+	useEffect(() => {
+		const obtenerInfoPokemon = async () => {
+			try {
+				const response = await fetch(url);
+				const data = await response.json();
+				setPokemonInfo(data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+
+		obtenerInfoPokemon();
+	}, [url]);
+
+	if (!pokemonInfo) {
+		return null;
+	}
+	return (
+		<Link href={'news'}>
+			<Card h='100%' w='369px'>
+				<CardBody>
+					<Image
+						borderRadius='lg'
+						src={
+							pokemonInfo.sprites.other.dream_world.front_default ||
+              pokemonInfo.sprites.other.home.front_default
+						}
+						alt={pokemonInfo.name}
+					/>
+					<Stack mt='6' spacing={3}>
+						<Text fontSize={'md'}>{pokemonInfo.name}</Text>
+						<Heading size={'15'}>TextoNormalon</Heading>
+						{pokemonInfo.abilities.map((ability) => (
+							<Text fontSize='sm' key={ability.ability.name}>
+								{ability.ability.name}
+							</Text>
+						))}
+					</Stack>
+				</CardBody>
+			</Card>
+		</Link>
+	);
+};
+
+interface Pokemon {
+  name: string;
+  url: string;
+}
+
+export const NewsByCategory = () => {
+	const [pokemonData, setPokemonData] = useState<Pokemon[]>([]);
+	const [numberItems, setNumberItems] = useState(0);
+	const pageSize = 12;
+	const [offset, setOffset] = useState<number>(0);
+
+	const comlumnCount = useBreakpointValue({ base: 1, sm: 2, md: 3 });
+	useEffect(() => {
+		const obtenerPokemones = async () => {
+			try {
+				const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${pageSize}`;
+				const response = await fetch(url);
+				const json = await response.json();
+				setNumberItems(json.count);
+				setPokemonData(json.results);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		obtenerPokemones();
+	}, [offset]);
+
+	return (
+		<Box maxW='1280' w='100%' m='0 auto'>
+			<Grid
+				templateColumns={`repeat(${comlumnCount},minmax(0,1fr))`}
+				gap={6}
+				p='2'
+			>
+				{pokemonData.map((pokemon) => (
+					<Cards key={pokemon.name} url={pokemon.url} />
+				))}
+			</Grid>
+		</Box>
+	);
+};
+
+export default memo(NewsByCategory);
