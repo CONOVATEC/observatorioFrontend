3a662d27c3fbd54fba2d3da160132987bf5096c8
Miki Tucto Leon <mick01leon@gmail.com>
2023-05-19 23:43:30 -0500

    Agregando el paginado a las tarjetas de noticias

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,21 @@
 import '../styles/globals.css';
+import '../styles/Pagination.css';
 import type { AppProps } from 'next/app';
 import { wrapper } from '../src/redux/store';
-import { ColorModeScript } from '@chakra-ui/react'
+import { ColorModeScript } from '@chakra-ui/react';
 import { ChakraProvider } from '@chakra-ui/react';
 import Head from 'next/head';
 import theme from '../src/utils/theme';
 
 export function App({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Head>
-        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      </Head>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<Head>
+				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			</Head>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	);
 }
 
 export default wrapper.withRedux(App);
--- src/containers/NewsByCategory/index.tsx
+++ src/containers/NewsByCategory/index.tsx
@@ -10,8 +10,10 @@ import {
 	Heading,
 	GridItem,
 	Button,
+	Box,
 } from '@chakra-ui/react';
 import { memo, useEffect, useState } from 'react';
+import { PaginationWrapper } from '../../components/PaginationWrapper';
 interface PokemonInfo {
   name: string;
   sprites: {
@@ -52,9 +54,9 @@ export const Cards: React.FC<PokemonCardProps> = ({ url }) => {
 		return null;
 	}
 	return (
-		<Link h='400' sx={{'&:hover': { textDecoration: 'none' } }}>
+		<Link h='400' sx={{ '&:hover': { textDecoration: 'none' } }}>
 			<Card h='100%'>
-				<CardBody p='20px'>
+				<CardBody p='20px' h='100%'>
 					<Image
 						borderRadius='lg'
 						src={
@@ -65,13 +67,14 @@ export const Cards: React.FC<PokemonCardProps> = ({ url }) => {
 						h='40%'
 					/>
 					<Stack mt='6' spacing={3}>
-						<Text fontSize={'md'}>{pokemonInfo.name}</Text>
-						<Heading size={'15'}>TextoNormalon</Heading>
-						{pokemonInfo.abilities.map((ability) => (
-							<Text fontSize='sm' key={ability.ability.name}>
-								{ability.ability.name}
-							</Text>
-						))}
+						<Text fontSize={'sm'}>Autor | dd-MM-yyyy</Text>
+						<Heading size='md'>Titulo Noticia</Heading>
+						<Text>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas
+              officia qui velit corrupti. Reprehenderit dolor sequi vitae ab, id
+              dolore quas sed enim quisquam sint! Consectetur earum assumenda
+              impedit deleniti.
+						</Text>
 					</Stack>
 				</CardBody>
 			</Card>
@@ -89,6 +92,12 @@ export const NewsByCategory = () => {
 	const [numberItems, setNumberItems] = useState(0);
 	const pageSize = 12;
 	const [offset, setOffset] = useState<number>(0);
+	const [currentPage, setCurrentPage] = useState<number>(1);
+
+	const handlePageChange = (current: number) => {
+		const newOffset = (current - 1) * pageSize;
+		setOffset(newOffset);
+	};
 
 	const comlumnCount = useBreakpointValue({ base: 1, sm: 2, md: 3 });
 	useEffect(() => {
@@ -104,10 +113,11 @@ export const NewsByCategory = () => {
 			}
 		};
 		obtenerPokemones();
+		setCurrentPage(offset / pageSize + 1);
 	}, [offset]);
 
 	return (
-		<GridItem maxW='1280' w='100%' m='0 auto'>
+		<GridItem maxW='1280' w='100%' m='0 auto' p='2'>
 			<Grid
 				templateColumns={`repeat(${comlumnCount},minmax(0,1fr))`}
 				gap={6}
@@ -117,6 +127,12 @@ export const NewsByCategory = () => {
 					<Cards key={pokemon.name} url={pokemon.url} />
 				))}
 			</Grid>
+			<PaginationWrapper
+				total={numberItems}
+				pageSize={pageSize}
+				current={currentPage}
+				onChange={handlePageChange}
+			/>
 		</GridItem>
 	);
 };
