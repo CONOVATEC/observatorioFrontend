bd20c76cd14a06651d01cf19d62496242130cd6f
Joseph <josephchero.jc@gmail.com>
2023-10-04 19:39:15 -0500

    update pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,5 +1,6 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { Flex, Button, Select, Text, ButtonGroup } from '@chakra-ui/react';
+import { Flex, Button, IconButton } from '@chakra-ui/react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@chakra-ui/icons';
 
 interface PaginationProps {
   totalPosts: number;
@@ -17,45 +18,128 @@ const Pagination = ({
   const totalPages = Math.ceil(totalPosts / postsPerPage);
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
-  const handlePageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setPage(Number(event.target.value));
-  };
+  if (totalPages <= 1) {
+    return <></>;
+  }
 
   return (
-    <Flex align='center' justify='center' mt={4}>
-      <ButtonGroup size='sm' isAttached>
-        {currentPage > 1 && (
-          <Button colorScheme='blue' onClick={() => setPage(currentPage - 1)}>
-            Anterior
-          </Button>
-        )}
-        <Select
-          value={currentPage}
-          onChange={handlePageChange}
-          width='auto'
-          variant='outline'
-          marginX={2}
-          size='sm'
-        >
-          {pageNumbers.map((pageNumber) => (
-            <option key={pageNumber} value={pageNumber}>
-              {pageNumber}
-            </option>
-          ))}
-        </Select>
-        {currentPage < totalPages && (
+    <Flex align='center' justify='center' mt={4} gap={3}>
+      <IconButton
+        variant={currentPage === 1 ? 'solid' : 'outline'}
+        isDisabled={currentPage === 1}
+        colorScheme='brand'
+        aria-label='Next Page'
+        icon={<ChevronLeftIcon />}
+        onClick={() => setPage(currentPage - 1)}
+        _dark={{
+          bg: currentPage === 1 ? '#2D3748' : undefined,
+          color: 'white',
+        }}
+        fontSize={'3xl'}
+      />
+      {totalPages <= 7 &&
+        pageNumbers.map((page, i) => (
           <Button
-            size='sm'
-            colorScheme='blue'
-            onClick={() => setPage(currentPage + 1)}
+            variant={'outline'}
+            key={page}
+            colorScheme={page === currentPage ? 'brand' : undefined}
+            onClick={() => setPage(page)}
+            fontWeight={'bold'}
           >
-            Siguiente
+            {page}
           </Button>
-        )}
-      </ButtonGroup>
-      <Text ml={4}>
-        Página {currentPage} de {totalPages}
-      </Text>
+        ))}
+      {totalPages > 7 && (
+        <>
+          {pageNumbers.slice(0, 1).map((page) => (
+            <Button
+              variant='outline'
+              key={page}
+              colorScheme={page === currentPage ? 'brand' : undefined}
+              onClick={() => setPage(page)}
+              fontWeight={'bold'}
+            >
+              {page}
+            </Button>
+          ))}
+          {currentPage >= 5 && (
+            <Button isDisabled fontSize={'3xl'}>
+              ...
+            </Button>
+          )}
+          {currentPage < 5 &&
+            pageNumbers.slice(1, 5).map((page) => (
+              <Button
+                variant='outline'
+                key={page}
+                colorScheme={page === currentPage ? 'brand' : undefined}
+                onClick={() => setPage(page)}
+                fontWeight={'bold'}
+              >
+                {page}
+              </Button>
+            ))}
+          {currentPage >= 5 &&
+            currentPage < totalPages - 3 &&
+            pageNumbers.slice(0, 3).map((_, index) => (
+              <Button
+                variant='outline'
+                key={index}
+                colorScheme={
+                  currentPage + (index - 1) === currentPage
+                    ? 'brand'
+                    : undefined
+                }
+                onClick={() => setPage(currentPage + (index - 1))}
+                fontWeight={'bold'}
+              >
+                {currentPage + (index - 1)}
+              </Button>
+            ))}
+
+          {currentPage < totalPages - 3 && (
+            <Button isDisabled fontSize={'3xl'}>
+              ...
+            </Button>
+          )}
+          {currentPage > totalPages - 4 &&
+            pageNumbers.slice(-5, -1).map((page, index) => (
+              <Button
+                variant='outline'
+                key={index}
+                colorScheme={page === currentPage ? 'brand' : undefined}
+                onClick={() => setPage(page)}
+                fontWeight={'bold'}
+              >
+                {page}
+              </Button>
+            ))}
+          {pageNumbers.slice(-1).map((page) => (
+            <Button
+              variant='outline'
+              key={page}
+              colorScheme={page === currentPage ? 'brand' : undefined}
+              onClick={() => setPage(page)}
+              fontWeight={'bold'}
+            >
+              {page}
+            </Button>
+          ))}
+        </>
+      )}
+      <IconButton
+        variant={currentPage === totalPages ? 'solid' : 'outline'}
+        isDisabled={currentPage === totalPages}
+        colorScheme='brand'
+        aria-label='Next Page'
+        icon={<ChevronRightIcon />}
+        onClick={() => setPage(currentPage + 1)}
+        _dark={{
+          bg: currentPage === totalPages ? '#2D3748' : undefined,
+          color: 'white',
+        }}
+        fontSize={'3xl'}
+      />
     </Flex>
   );
 };
