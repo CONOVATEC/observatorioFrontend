5c5d04d2d563c60cdc849562e2d9fb8ad145fe8e
Diana2x <dianaclxcoe@gmail.com>
2023-12-02 04:15:26 +0100

    Centered div in about us section

--- src/containers/BannerAboutSection/index.tsx
+++ src/containers/BannerAboutSection/index.tsx
@@ -1,28 +1,33 @@
-import { HStack, Text, VStack } from '@chakra-ui/react'
-import React from 'react'
-import { useAboutObservatoryInfo } from './hooks'
+import { Center, HStack, Text, VStack } from '@chakra-ui/react';
+import React from 'react';
+import { useAboutObservatoryInfo } from './hooks';
 
 function BannerAboutSection() {
-  const { description } = useAboutObservatoryInfo()
+  const { description } = useAboutObservatoryInfo();
 
   return (
     <HStack
       paddingX={'8px'}
       height={'45vh'}
-      backgroundImage={'url("https://res.cloudinary.com/df5nwnlnu/image/upload/v1671396068/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/NOSOTROS/fondo-quienes_somos_V2_xtb3e7.png")'}>
+      backgroundImage={
+        'url("https://res.cloudinary.com/df5nwnlnu/image/upload/v1671396068/observatorio/PIEZAS%20GR%C3%81FICAS%20-%20OBSERVATORIO%20JOVEN/NOSOTROS/fondo-quienes_somos_V2_xtb3e7.png")'
+      }
+      align={'center'}
+      justify={'center'}
+    >
       <VStack
         paddingX={{ base: '10%', md: '19%' }}
         spacing={{ base: 5, md: 10 }}
-        textColor='white'>
+        textColor='white'
+        textAlign='center'
+      >
         <Text fontWeight={'bold'} fontSize={'3xl'}>
           ¿Quiénes somos?
         </Text>
-        <Text>
-          {description}
-        </Text>
+        <Text>{description}</Text>
       </VStack>
     </HStack>
-  )
+  );
 }
 
-export default BannerAboutSection
+export default BannerAboutSection;
