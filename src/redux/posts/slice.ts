66171dcf404cc1a5592fc3d92367abbd81b69b0e
Joseph <josephchero.jc@gmail.com>
2023-12-06 21:30:18 -0500

    Fix formatting in posts slice and theme

--- src/redux/posts/slice.ts
+++ src/redux/posts/slice.ts
@@ -1,24 +1,24 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { APIS_URLS } from '../../../config'
-import { PostFilter } from '../../types'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { APIS_URLS } from '../../../config';
+import { PostFilter } from '../../types';
 
-export const postsApi:any = createApi({
+export const postsApi: any = createApi({
   reducerPath: 'postsApi',
   baseQuery: fetchBaseQuery({ baseUrl: APIS_URLS.OBSERVATORIO_BACK }),
-  endpoints: builder => ({
+  endpoints: (builder) => ({
     getPostsApi: builder.query({
       query: (filter?: PostFilter) => {
         if (filter) {
           const postFilterRecord: Record<string, string> = {
             category: filter.category,
-            page: filter.page || "3",
+            page: filter.page || '3',
           };
-          return `/api/v1/posts?included=user,category,tags&perPage=${postFilterRecord.page}&filter[category]=${postFilterRecord.category}`
+          return `/api/v1/posts?included=user,category,tags&perPage=${postFilterRecord.page}&filter[category]=${postFilterRecord.category}`;
         }
-        return `/api/v1/posts?included=user,category,tags&perPage=3`
-      }
-    })
-  })
-})
+        return '/api/v1/posts?included=user,category,tags&perPage=3';
+      },
+    }),
+  }),
+});
 
-export const { useGetPostsApiQuery, reducer } = postsApi
+export const { useGetPostsApiQuery, reducer } = postsApi;
--- src/utils/theme.tsx
+++ src/utils/theme.tsx
@@ -1,18 +1,18 @@
-import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
   initialColorMode: 'light',
   useSystemColorMode: true,
-}
+};
 
 const theme = extendTheme({
   ...config,
   colors: {
     brand: {
-      500: "#9537D3",
-      600: "#9537D3"
+      500: '#9537D3',
+      600: '#9537D3',
     },
   },
-})
+});
 
-export default theme
+export default theme;
