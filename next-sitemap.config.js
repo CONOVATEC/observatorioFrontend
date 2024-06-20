423c85e19266947f9a738b77263c5ae2ff50639a
joseph <josephchero.jc@gmail.com>
2024-06-19 23:42:15 -0500

    chore: Update siteUrl and Host in next-sitemap.config.js and robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://observatoriojovenperu.com.pe/peru/',
-    generateRobotsTxt: true, 
-}
+  siteUrl:
+    process.env.SITE_URL || 'https://observatoriojovenperu.com.pe/perudev/',
+  generateRobotsTxt: true,
+};
