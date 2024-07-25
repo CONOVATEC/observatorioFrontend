96f51a17f86ba98f4cb558504f3b1658e9abbb3f
joseph <josephchero.jc@gmail.com>
2024-07-24 22:46:01 -0500

    chore: Update .gitignore to exclude .next.zip file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
+const isProduction = process.env.ENVIROMENT_SLUG === 'production';
+const siteUrl = isProduction
+  ? 'https://observatoriojovenperu.com.pe/peru/'
+  : 'https://observatoriojovenperu.com.pe/perudev/';
 module.exports = {
-  siteUrl:
-    process.env.SITE_URL || 'https://observatoriojovenperu.com.pe/perudev/',
+  siteUrl,
   generateRobotsTxt: true,
 };
